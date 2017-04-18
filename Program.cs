edbe03acb0c4aedd5f320067cf707019635b5946
Lalit Lakum <Lakumlalit@gmail.com>
2017-04-18 15:43:26 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hello-Git
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Start writing code..
+        }
+    }
+}
